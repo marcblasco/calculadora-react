5b39538ef9f706bea75c87897d85c0af47bb50c5
Marc Blasco <marcblasconarro@gmail.com>
2022-08-05 11:12:53 +0200

    creating button component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import freeCodeCampLogo from './img/freecode.png';
+import Boton from './componentes/Boton';
 
 function App() {
   return (
     <div className='App'>
       <div className='logo-contenedor'>
       <img 
-          src={freeCodeCampLogo} className='freecodecamp-logo'   alt='Logo ' />
+          src={freeCodeCampLogo} className='freecodecamp-logo' alt='Logo ' />
       </div>
-     
+      <div className='contenedor-calculadora'>
+        <div className='fila'>
+          <Boton>1</Boton>
+        </div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
     </div>
   );
 }
